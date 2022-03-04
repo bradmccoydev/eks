6fde65cd69ff63fbb84590b0b62bb895d9ad2b56
bradmccoydev <bradmccoydev@gmail.com>
2022-03-04 14:46:53 +1000

    update network
    Signed-off-by: bradmccoydev <bradmccoydev@gmail.com>

--- src/network.tf
+++ src/network.tf
@@ -1,87 +1,117 @@
-resource "aws_vpc" "default" {
-  cidr_block           = var.cidr_block
-  enable_dns_support   = true
-  enable_dns_hostnames = true
+# ---------------------------------------------------------------------------------------------------------------------
+# AWS Network
+# ---------------------------------------------------------------------------------------------------------------------
+
+module "aws_vpc" {
+  source = "github.com/bradmccoydev/terraform-modules//aws/aws_vpc?ref=0.0.1"
+  cidr_block = var.cidr_block
   tags = var.tags
-}
+} 
 
-resource "aws_internet_gateway" "default" {
-  vpc_id = aws_vpc.default.id
-  tags = var.tags
-}
+# resource "aws_security_group" "app_servers" {
+#   name        = "app-servers"
+#   description = "For application servers"
+#   vpc_id      = data.aws_vpc.default.id
+# }
 
-resource "aws_subnet" "public_1" {
-  vpc_id                  = aws_vpc.default.id
-  cidr_block              = var.public_subnet_cidr_block_1
-  availability_zone       = var.availability_zone_1
-  map_public_ip_on_launch = true
-}
+# resource "aws_security_group_rule" "allow_access" {
+#   type                     = "ingress"
+#   from_port                = module.aurora.this_rds_cluster_port
+#   to_port                  = module.aurora.this_rds_cluster_port
+#   protocol                 = "tcp"
+#   source_security_group_id = aws_security_group.app_servers.id
+#   security_group_id        = module.aurora.this_security_group_id
+# }
 
-resource "aws_subnet" "public_2" {
-  vpc_id                  = aws_vpc.default.id
-  cidr_block              = var.public_subnet_cidr_block_2
-  availability_zone       = var.availability_zone_2
-  map_public_ip_on_launch = true
-}
-
-resource "aws_subnet" "private_1" {
-  vpc_id            = aws_vpc.default.id
-  cidr_block        = var.private_subnet_cidr_block_1
-  availability_zone = var.availability_zone_1
-}
-
-resource "aws_subnet" "private_2" {
-  vpc_id            = aws_vpc.default.id
-  cidr_block        = var.private_subnet_cidr_block_2
-  availability_zone = var.availability_zone_2
-}
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.default.id
-}
-
-resource "aws_route" "public" {
-  route_table_id         = aws_route_table.public.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.default.id
-}
-
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.default.id
-}
-
-resource "aws_route" "private" {
-  route_table_id         = aws_route_table.private.id
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.default.id
-}
-
-resource "aws_nat_gateway" "default" {
-  depends_on = [aws_internet_gateway.default]
-  allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public_1.id
-}
-
-resource "aws_eip" "nat" {
+module "aws_eip" {
+  source = "./modules/aws/network/elastic_ip/terraform"
   vpc = true
-}
+} 
 
-resource "aws_route_table_association" "private_1" {
-  subnet_id      = aws_subnet.private_1.id
-  route_table_id = aws_route_table.private.id
-}
+module "aws_internet_gateway" {
+  source  = "./modules/aws/network/internet_gateway/terraform"
+  vpc_id  = module.aws_vpc.id
+  tags    = var.tags
+} 
 
-resource "aws_route_table_association" "private_2" {
-  subnet_id      = aws_subnet.private_2.id
-  route_table_id = aws_route_table.private.id
-}
+module "aws_subnet_public_1" {
+  source = "./modules/aws/network/subnet/terraform"
+  vpc_id             = module.aws_vpc.id
+  cidr_block         = var.public_subnet_cidr_block_1
+  availability_zone  = var.availability_zone_1
+} 
 
-resource "aws_route_table_association" "public_1" {
-  subnet_id      = aws_subnet.public_1.id
-  route_table_id = aws_route_table.public.id
-}
+module "aws_subnet_public_2" {
+  source = "./modules/aws/network/subnet/terraform"
+  vpc_id             = module.aws_vpc.id
+  cidr_block         = var.public_subnet_cidr_block_2
+  availability_zone  = var.availability_zone_2
+} 
 
-resource "aws_route_table_association" "public_2" {
-  subnet_id      = aws_subnet.public_2.id
-  route_table_id = aws_route_table.public.id
-}
+module "aws_subnet_private_1" {
+  source = "./modules/aws/network/subnet/terraform"
+  vpc_id             = module.aws_vpc.id
+  cidr_block         = var.private_subnet_cidr_block_1
+  availability_zone  = var.availability_zone_1
+} 
+
+module "aws_subnet_private_2" {
+  source = "./modules/aws/network/subnet/terraform"
+  vpc_id             = module.aws_vpc.id
+  cidr_block         = var.private_subnet_cidr_block_2
+  availability_zone  = var.availability_zone_2
+} 
+
+module "aws_route_table_public" {
+  source  = "./modules/aws/network/route_table/terraform"
+  vpc_id  = module.aws_vpc.id
+} 
+
+module "aws_route_public" {
+  source  = "./modules/aws/network/route/terraform"
+  route_table_id         = module.aws_route_table_public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = module.aws_internet_gateway.id
+} 
+
+module "aws_route_table_private" {
+  source  = "./modules/aws/network/route_table/terraform"
+  vpc_id  = module.aws_vpc.id
+} 
+
+module "aws_route_private" {
+  source  = "./modules/aws/network/route/terraform"
+  route_table_id         = module.aws_route_table_private.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = module.aws_internet_gateway.id
+} 
+
+module "aws_nat_gateway" {
+  source  = "./modules/aws/network/nat_gateway/terraform"
+  allocation_id = module.aws_eip.id
+  subnet_id     = module.aws_subnet_public_1.id
+} 
+
+module "aws_route_table_association_private_1" {
+  source  = "./modules/aws/network/aws_route_table_association/terraform"
+  subnet_id     = module.aws_subnet_private_1.id
+  route_table_id = module.aws_route_private.id
+} 
+
+module "aws_route_table_association_private_2" {
+  source  = "./modules/aws/network/aws_route_table_association/terraform"
+  subnet_id     = module.aws_subnet_private_2.id
+  route_table_id = module.aws_route_private.id
+} 
+
+module "aws_route_table_association_public_1" {
+  source  = "./modules/aws/network/aws_route_table_association/terraform"
+  subnet_id     = module.aws_subnet_public_1.id
+  route_table_id = module.aws_route_public.id
+} 
+
+module "aws_route_table_association_public_2" {
+  source  = "./modules/aws/network/aws_route_table_association/terraform"
+  subnet_id     = module.aws_subnet_public_2.id
+  route_table_id = module.aws_route_public.id
+} 
--- src/provider.tf
+++ src/provider.tf
@@ -2,14 +2,14 @@ terraform {
   backend "s3" {
     bucket         = "mentoring-eks-terraform"
     key            = "mentoring-eks.tfstate"
-    region         = "us-west-2"
+    region         = "ap-southeast-2"
     dynamodb_table = "terraform_lock"
     encrypt        = true
   }
 }
 
 provider "aws" {
-  region = "ap-southeast-1"
+  region = "ap-southeast-2"
 }
 
 provider "random" {
